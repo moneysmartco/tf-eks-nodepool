4e06ed93688ac63da49c4fa756652eb5594d9eab
alex arockiaraj <alex@francium.tech>
2021-01-07 10:47:32 +0530

    terraform 0.12 update

--- spotinst.tf
+++ spotinst.tf
@@ -1,25 +1,25 @@
 resource "spotinst_ocean_aws" "spotinst_auto_scaling" {
-  count = "${var.spotinst_enable ? 1 : 0}"
+  count = var.spotinst_enable ? 1 : 0
 
-  region        = "${var.spotinst_region}"
+  region        = var.spotinst_region
   name          = "${var.project_name}-${var.env}"
   controller_id = "${var.project_name}-${var.env}"
 
   # Instance type & counts
-  whitelist        = "${split(",", var.spotinst_whitelist)}"
-  min_size         = "${var.spotinst_min_size}"
-  max_size         = "${var.spotinst_max_size}"
-  draining_timeout = "${var.spotinst_draining_timeout}"
+  whitelist        = split(",", var.spotinst_whitelist)
+  min_size         = var.spotinst_min_size
+  max_size         = var.spotinst_max_size
+  draining_timeout = var.spotinst_draining_timeout
 
   # Networking
-  subnet_ids      = "${split(",", var.private_subnet_ids)}"
-  image_id        = "${data.aws_ssm_parameter.eks_node.value}"
-  security_groups = ["${var.worker_node_security_group_id}"]
+  subnet_ids      = split(",", var.private_subnet_ids)
+  image_id        = data.aws_ssm_parameter.eks_node.value
+  security_groups = [var.worker_node_security_group_id]
 
   # Metadata
-  key_name             = "${var.deploy_key_name}"
-  user_data            = "${data.template_file.user_data.rendered}"
-  iam_instance_profile = "${var.worker_node_instance_profile_name}"
+  key_name             = var.deploy_key_name
+  user_data            = data.template_file.user_data.rendered
+  iam_instance_profile = var.worker_node_instance_profile_name
   monitoring           = true                                       # Detailed monitoring
 
   autoscaler {
@@ -30,43 +30,43 @@ resource "spotinst_ocean_aws" "spotinst_auto_scaling" {
   tags = [
     {
       key   = "Name"
-      value = "${var.spotinst_tags_name}"
+      value = var.spotinst_tags_name
     },
     {
       key   = "Country"
-      value = "${var.spotinst_tags_country}"
+      value = var.spotinst_tags_country
     },
     {
       key   = "Environment"
-      value = "${var.spotinst_tags_environment}"
+      value = var.spotinst_tags_environment
     },
     {
       key   = "Repository"
-      value = "${var.spotinst_tags_repository}"
+      value = var.spotinst_tags_repository
     },
     {
       key   = "Owner"
-      value = "${var.spotinst_tags_owner}"
+      value = var.spotinst_tags_owner
     },
     {
       key   = "Department"
-      value = "${var.spotinst_tags_department}"
+      value = var.spotinst_tags_department
     },
     {
       key   = "Team"
-      value = "${var.spotinst_tags_team}"
+      value = var.spotinst_tags_team
     },
     {
       key   = "Product"
-      value = "${var.spotinst_tags_product}"
+      value = var.spotinst_tags_product
     },
     {
       key   = "Project"
-      value = "${var.spotinst_tags_project}"
+      value = var.spotinst_tags_project
     },
     {
       key   = "Stack"
-      value = "${var.spotinst_tags_stack}"
+      value = var.spotinst_tags_stack
     },
     {
       key   = "kubernetes.io/cluster/${var.eks_cluster_name}"
