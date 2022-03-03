9df6705f83a28b659e93b26df96e0091acf02d4c
Alex <alex@MacBook-Air.local>
2022-03-03 16:42:51 +0530

    Updated provider versions

--- versions.tf
+++ versions.tf
@@ -1,16 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 4.3.0"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
+      version = "~> 3.1.0"
     }
     spotinst = {
-      source = "spotinst/spotinst"
+      source  = "spotinst/spotinst"
+      version = "~> 1.68.0"
     }
     template = {
-      source = "hashicorp/template"
+      source  = "hashicorp/template"
+      version = "~> 2.2.0"
     }
   }
   required_version = ">= 0.13"
