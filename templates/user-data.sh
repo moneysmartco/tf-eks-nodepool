d31a44a9ba442e8cfe0969f177adb4127c42e554
Alex <alex@MacBook-Air.local>
2023-10-11 17:48:55 +0530

    Granted the super user access

--- templates/user-data.sh
+++ templates/user-data.sh
@@ -3,16 +3,16 @@ set -o xtrace
 
 if grep -q imageGCHighThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json;
 then
-    sed -i 's/"imageGCHighThresholdPercent":.*/"imageGCHighThresholdPercent": 50,/' /etc/kubernetes/kubelet/kubelet-config.json
+    sudo sed -i 's/"imageGCHighThresholdPercent":.*/"imageGCHighThresholdPercent": 50,/' /etc/kubernetes/kubelet/kubelet-config.json
 else
-    sed -i '/"apiVersion*/a \ \ "imageGCHighThresholdPercent": 50,' /etc/kubernetes/kubelet/kubelet-config.json
+    sudo sed -i '/"apiVersion*/a \ \ "imageGCHighThresholdPercent": 50,' /etc/kubernetes/kubelet/kubelet-config.json
 fi
 
 if grep -q imageGCLowThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json;
 then
-    sed -i 's/"imageGCLowThresholdPercent":.*/"imageGCLowThresholdPercent": 40,/' /etc/kubernetes/kubelet/kubelet-config.json
+    sudo sed -i 's/"imageGCLowThresholdPercent":.*/"imageGCLowThresholdPercent": 40,/' /etc/kubernetes/kubelet/kubelet-config.json
 else
-    sed -i '/"apiVersion*/a \ \ "imageGCLowThresholdPercent": 40,' /etc/kubernetes/kubelet/kubelet-config.json
+    sudo sed -i '/"apiVersion*/a \ \ "imageGCLowThresholdPercent": 40,' /etc/kubernetes/kubelet/kubelet-config.json
 fi
 
 /etc/eks/bootstrap.sh ${eks_cluster_name} --container-runtime containerd
