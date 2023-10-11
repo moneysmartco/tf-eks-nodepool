97115a0e7cbdb40fd66766fb84ee2f801affb9f3
Alex <alex@MacBook-Air.local>
2023-10-11 18:55:08 +0530

    Updated the script

--- templates/user-data.sh
+++ templates/user-data.sh
@@ -2,19 +2,18 @@
 set -o xtrace
 
 LOG_FILE="/var/log/eks-bootstrap.log"
-
 {
-    if grep -q imageGCHighThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json; then
-        sudo sed -i 's/"imageGCHighThresholdPercent":.*/"imageGCHighThresholdPercent": 50,/' /etc/kubernetes/kubelet/kubelet-config.json
-    else
-        sudo sed -i '/"apiVersion*/a \ \ "imageGCHighThresholdPercent": 50,' /etc/kubernetes/kubelet/kubelet-config.json
-    fi
+# Inject imageGCHighThresholdPercent value unless it has already been set.
+if ! grep -q imageGCHighThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json;
+then
+    sudo sed -i '/"apiVersion*/a \ \ "imageGCHighThresholdPercent": 50,' /etc/kubernetes/kubelet/kubelet-config.json
+fi
 
-    if grep -q imageGCLowThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json; then
-        sudo sed -i 's/"imageGCLowThresholdPercent":.*/"imageGCLowThresholdPercent": 40,/' /etc/kubernetes/kubelet/kubelet-config.json
-    else
-        sudo sed -i '/"apiVersion*/a \ \ "imageGCLowThresholdPercent": 40,' /etc/kubernetes/kubelet/kubelet-config.json
-    fi
+# Inject imageGCLowThresholdPercent value unless it has already been set.
+if ! grep -q imageGCLowThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json;
+then
+    sudo sed -i '/"imageGCHigh*/a \ \ "imageGCLowThresholdPercent": 40,' /etc/kubernetes/kubelet/kubelet-config.json
+fi
 
     /etc/eks/bootstrap.sh ${eks_cluster_name} --container-runtime containerd
 } >> "$LOG_FILE" 2>&1
