9c74528927504096c90d6ea392c2f46d735a7620
Alex <alex@MacBook-Air.local>
2023-10-11 18:19:01 +0530

    Added logfile to check the issue

--- templates/user-data.sh
+++ templates/user-data.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -o xtrace
 
-if grep -q imageGCHighThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json;
-then
-    sudo sed -i 's/"imageGCHighThresholdPercent":.*/"imageGCHighThresholdPercent": 50,/' /etc/kubernetes/kubelet/kubelet-config.json
-else
-    sudo sed -i '/"apiVersion*/a \ \ "imageGCHighThresholdPercent": 50,' /etc/kubernetes/kubelet/kubelet-config.json
-fi
+LOG_FILE="/var/log/eks-bootstrap.log"
 
-if grep -q imageGCLowThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json;
-then
-    sudo sed -i 's/"imageGCLowThresholdPercent":.*/"imageGCLowThresholdPercent": 40,/' /etc/kubernetes/kubelet/kubelet-config.json
-else
-    sudo sed -i '/"apiVersion*/a \ \ "imageGCLowThresholdPercent": 40,' /etc/kubernetes/kubelet/kubelet-config.json
-fi
+{
+    if grep -q imageGCHighThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json; then
+        sudo sed -i 's/"imageGCHighThresholdPercent":.*/"imageGCHighThresholdPercent": 50,/' /etc/kubernetes/kubelet/kubelet-config.json
+    else
+        sudo sed -i '/"apiVersion*/a \ \ "imageGCHighThresholdPercent": 50,' /etc/kubernetes/kubelet/kubelet-config.json
+    fi
 
-/etc/eks/bootstrap.sh ${eks_cluster_name} --container-runtime containerd
+    if grep -q imageGCLowThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json; then
+        sudo sed -i 's/"imageGCLowThresholdPercent":.*/"imageGCLowThresholdPercent": 40,/' /etc/kubernetes/kubelet/kubelet-config.json
+    else
+        sudo sed -i '/"apiVersion*/a \ \ "imageGCLowThresholdPercent": 40,' /etc/kubernetes/kubelet/kubelet-config.json
+    fi
+
+    /etc/eks/bootstrap.sh ${eks_cluster_name} --container-runtime containerd
+} >> "$LOG_FILE" 2>&1
