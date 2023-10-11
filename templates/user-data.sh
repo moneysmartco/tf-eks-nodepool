78bf3d529586f812f9ab5f07f6e40acec9ca0ddb
Alex <alex@MacBook-Air.local>
2023-10-11 16:31:56 +0530

    Updated script to remove image cache

--- templates/user-data.sh
+++ templates/user-data.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 set -o xtrace
+if ! grep -q imageGCHighThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json;
+then
+    sed -i '/"apiVersion*/a \ \ "imageGCHighThresholdPercent": 70,' /etc/kubernetes/kubelet/kubelet-config.json
+fi
+if ! grep -q imageGCLowThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json;
+then
+    sed -i '/"imageGCHigh*/a \ \ "imageGCLowThresholdPercent": 50,' /etc/kubernetes/kubelet/kubelet-config.json
+fi
 /etc/eks/bootstrap.sh ${eks_cluster_name} ${bootstrap_arguments} --container-runtime containerd
