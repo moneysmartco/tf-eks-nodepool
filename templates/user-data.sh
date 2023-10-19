7d3ef9f66c43ca0984466e7ff75ed8c1dc3d6bb7
Alex <alex@MacBook-Air.local>
2023-10-19 11:13:02 +0530

    Updated garbage collection

--- templates/user-data.sh
+++ templates/user-data.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 set -o xtrace
-
-LOG_FILE="/var/log/eks-bootstrap.log"
-{
-# Inject imageGCHighThresholdPercent value unless it has already been set.
-if ! grep -q imageGCHighThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json;
-then
-    sudo sed -i '/"apiVersion*/a \ \ "imageGCHighThresholdPercent": 50,' /etc/kubernetes/kubelet/kubelet-config.json
-fi
-
-# Inject imageGCLowThresholdPercent value unless it has already been set.
-if ! grep -q imageGCLowThresholdPercent /etc/kubernetes/kubelet/kubelet-config.json;
-then
-    sudo sed -i '/"imageGCHigh*/a \ \ "imageGCLowThresholdPercent": 40,' /etc/kubernetes/kubelet/kubelet-config.json
-fi
-
-    /etc/eks/bootstrap.sh ${eks_cluster_name} --container-runtime containerd
-} >> "$LOG_FILE" 2>&1
+/etc/eks/bootstrap.sh ${eks_cluster_name} --container-runtime containerd --kubelet-extra-args '--image-gc-high-threshold=50 --image-gc-low-threshold=40  --minimum-container-ttl-duration=0s'
