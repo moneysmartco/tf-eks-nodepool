69d1b45267e1eb38ad889de425f52a2e4dc9079b
Alex <alex@MacBook-Air.local>
2023-10-19 11:14:36 +0530

    typo

--- templates/user-data.sh
+++ templates/user-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -o xtrace
-/etc/eks/bootstrap.sh ${eks_cluster_name} --container-runtime containerd --kubelet-extra-args '--image-gc-high-threshold=50 --image-gc-low-threshold=40  --minimum-container-ttl-duration=0s'
+/etc/eks/bootstrap.sh ${eks_cluster_name} ${bootstrap_arguments} --container-runtime containerd --kubelet-extra-args '--image-gc-high-threshold=50 --image-gc-low-threshold=40  --minimum-container-ttl-duration=0s'
